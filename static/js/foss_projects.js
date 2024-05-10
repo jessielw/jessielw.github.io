5e4408c9aa06dd63fe7a1e4b87319b0ef8812042
jlw4049 <jlw_4049@hotmail.com>
2024-05-09 20:32:56 -0400

    feat: complete initial foss

---
+++ static/js/foss_projects.js
@@ -0,0 +1,10 @@
+const comparisonImage1 = document.getElementById("comparison-img-1");
+const comparisonImage2 = document.getElementById("comparison-img-2");
+
+comparisonImage1.addEventListener("click", function () {
+  window.open("https://images2.imgbox.com/59/bd/woQpbay2_o.png", "_blank");
+});
+
+comparisonImage2.addEventListener("click", function () {
+  window.open("https://images2.imgbox.com/96/24/WzCtO3dY_o.png", "_blank");
+});
